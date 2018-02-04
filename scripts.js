534480c886f838da2210cc9123b8f32d02fb0c2e
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-04 21:18:11 +0100

    Change js content

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0)
-    return 'Nieprawidlowe dane';
+	if (a <= 0 || h <= 0) { 
+    	return 'Nieprawidlowe dane';
+	}
 	var triangleArea = (a*h/2);
 	    console.log ('Pole trójkąta = ');
-	return triangleArea;    
+		return triangleArea;
 }
 	var triangle1Area = getTriangleArea(10, 15);
-	console.log(triangle1Area);
+		console.log(triangle1Area);
 	var triangle2Area = getTriangleArea(-1, 22);
-	console.log(triangle2Area);
+		console.log(triangle2Area);
 	var triangle3Area = getTriangleArea(3, 7);
-	console.log(triangle3Area);
+		console.log(triangle3Area);
